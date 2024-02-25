bb1ac76d8ab0e092f77d2278bd831692583efa23
Josenaldo Dantas <josenaldo.dantas@yahoo.com>
2024-02-24 23:58:02 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,14 @@
+    """
+    
+    Docstring 
+    posso escrever o que quiser 
+    dentro desse espaço
+
+     
+    """
+""" Usar para escrever notas """
+
+# Permite escrever um comentário
+print(123) # frente
+# abaixo
+print(123)
